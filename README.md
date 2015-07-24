93a629ece247194a6b058bd8a8b68624a12853e0
Monte Goulding <monte@sweattechnologies.com>
2015-07-24 16:37:45 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+#mergZumero
+
+A [LiveCode](http://runrev.com) [Zumero](http://zumero.com) SQL data sync external.
+
+##Author
+
+[Monte Goulding](mailto:monte@goulding.ws)
+
+Support the project by using me as a [consultant](http://goulding.ws) or by buying [mergExt](http://mergext.com)
+
+mergZumero uses the Zumero SDK which must be downloaded directly from [Zumero](http://zumero.com/dev-center/zss/) and installed into a subdirectory named Zumero in order to build the external.
+
+##License
+This external is available under the MIT license.
+
+##Support
+Ask it on [StackOverflow](http://stackoverflow.com) and it shall be answered.
+
+##Binaries
+Binaries for this external are unfortunately not available publicly due to the license on the Zumero SDK prohibiting distribution. If you are having trouble building the external email [Monte Goulding](mailto:monte@goulding.ws) and the external can be built against the most recent SDK for a small fee.
+
+##Installation
+
+Look [here](http://www.youtube.com/watch?v=zFTsVu7_QOI) for installation instructions for iOS.
---
+++ mergZumero/mergZumero.m
@@ -0,0 +1,124 @@
+//
+//  mergZumero.mm
+//  mergZumero
+//
+//  Created by Monte Goulding on 24/07/2015.
+//  Copyright 2015 M E R Goulding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <LiveCode.h>
+#import <ZumeroSync.h>
+
+static bool isSyncing = false;
+
+NSString * mergZumeroSync(NSString * pFileName, NSString * pServerURL, NSString * pRemote, NSString * pAuthSchemeJSON, NSString * pUser, NSString * pPassword)
+{
+    if (isSyncing)
+        return @"Already syncing";
+    
+    if ([pAuthSchemeJSON isEqualToString:@""]) {
+        pAuthSchemeJSON = nil;
+    }
+    
+    if ([pUser isEqualToString:@""]) {
+        pUser = nil;
+    }
+    
+    if ([pPassword isEqualToString:@""]) {
+        pPassword = nil;
+    }
+    
+    __block LCObjectRef me;
+    LCContextMe(&me);
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,
+                                             (unsigned long)NULL), ^(void) {
+        NSError * t_error = nil;
+        
+        BOOL ok = [ZumeroSync Sync:pFileName cipherKey:nil serverUrl:pServerURL remote:pRemote authSchemeJS:pAuthSchemeJSON user:pUser password:pPassword error: &t_error];
+    
+        isSyncing = false;
+        
+        if (!ok || t_error != nil)
+            LCObjectPost(me, "mergZumeroSyncError", "S", t_error != nil ? t_error.localizedDescription : @"unknown error" );
+        else
+            LCObjectPost(me, "mergZumeroSyncCompleted", "");
+        
+    });
+    
+    
+    return @"";
+}
+
+bool mergZumeroIsSyncing(void)
+{
+    return isSyncing;
+}
+
+NSString * mergZumeroQuarantineSinceLastSync(NSString * pFileName)
+{
+    
+    NSError * t_error = nil;
+    
+    sqlite3_int64 t_ID;
+    
+    BOOL ok = [ZumeroSync QuarantineSinceLastSync:pFileName cipherKey:nil pqid:&t_ID error:&t_error];
+    
+    if (!ok || t_error != nil)
+        return t_error != nil ? t_error.localizedDescription : @"unknown error";
+    
+    return [NSString stringWithFormat:@"%lld", t_ID];
+}
+
+NSString * mergZumeroSyncQuarantine(NSString * pFileName, NSString * pQuarantineID, NSString * pServerURL, NSString * pRemote, NSString * pAuthSchemeJSON, NSString * pUser, NSString * pPassword)
+{
+    if (isSyncing)
+        return @"Already syncing";
+    
+    if ([pAuthSchemeJSON isEqualToString:@""]) {
+        pAuthSchemeJSON = nil;
+    }
+    
+    if ([pUser isEqualToString:@""]) {
+        pUser = nil;
+    }
+    
+    if ([pPassword isEqualToString:@""]) {
+        pPassword = nil;
+    }
+    
+    __block LCObjectRef me;
+    LCContextMe(&me);
+    
+    __block sqlite3_int64 t_ID;
+    if (sscanf([pQuarantineID cStringUsingEncoding: NSMacOSRomanStringEncoding], " %lld ", &t_ID) != 1)
+        return @"Could not parse quarantine ID";
+    
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,
+                                             (unsigned long)NULL), ^(void) {
+        NSError * t_error = nil;
+        
+        BOOL t_partial = YES;
+        
+        
+        while (t_partial)
+        {
+            BOOL ok = [ZumeroSync SyncQuarantine:pFileName cipherKey:nil qid:t_ID serverUrl:pServerURL remote:pRemote authSchemeJS:pAuthSchemeJSON user:pUser password:pPassword partial:&t_partial error:&t_error];
+            if (t_error != nil)
+                break;
+        }
+        
+        isSyncing = false;
+        
+        if (t_error != nil)
+            LCObjectPost(me, "mergZumeroSyncError", "S", t_error != nil ? t_error.localizedDescription : @"unknown error" );
+        else
+            LCObjectPost(me, "mergZumeroSyncCompleted", "");
+        
+    });
+    
+    
+    return @"";
+}
